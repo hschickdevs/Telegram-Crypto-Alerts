426848911b546bf0deab5872ebd792b6812d008e
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2023-11-09 17:17:41 -0600

    Add Python version checking

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,2 +1,8 @@
 # Version 2.0.0 of the bot includes full functionality and documenation
 __version__ = "2.0.0"
+
+# Assert that Python version >= 3.9, or else they receive the subscription error for type hints
+import sys
+
+if sys.version_info < (3, 9):
+    raise Exception("Python version must be >= 3.9")
